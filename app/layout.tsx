5d68d4037fa192fc173bc13af564d0bc8bc9350f
Zananotlie <zanafaeroqzana@gmail.com>
2025-07-04 02:25:24 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'FLKRDMUSLIMS',
+  description: 'Created with FLKRD AI',
+  generator: 'ZANA AI ',
 }
 
 export default function RootLayout({
